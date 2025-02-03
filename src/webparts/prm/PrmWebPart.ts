1ca7391fa5fe6f0f419749fd055767732035a56c
rezmes <rezmes@gmail.com>
2025-02-03 15:07:40 +0330

    PrmWebpart.ts created

--- src/webparts/prm/PrmWebPart.ts
+++ src/webparts/prm/PrmWebPart.ts
@@ -1,39 +1,23 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
-import {
-  BaseClientSideWebPart,
-  IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-webpart-base';
-
+import {IPropertyPaneConfiguration, PropertyPaneTextField, BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import * as strings from 'PrmWebPartStrings';
-import Prm from './components/Prm';
-import { IPrmProps } from './components/IPrmProps';
-
-export interface IPrmWebPartProps {
-  description: string;
-}
+import ProjectRequestForm from './components/ProjectRequestForm';
+import { IPrmWebPartProps } from './IPrmWebPartProps';
 
 export default class PrmWebPart extends BaseClientSideWebPart<IPrmWebPartProps> {
-
   public render(): void {
-    const element: React.ReactElement<IPrmProps > = React.createElement(
-      Prm,
-      {
-        description: this.properties.description
-      }
-    );
+    const element: React.ReactElement<IPrmWebPartProps> = React.createElement(ProjectRequestForm, {
+      spHttpClient: this.context.spHttpClient,
+      siteUrl: this.context.pageContext.web.absoluteUrl
+    });
 
     ReactDom.render(element, this.domElement);
   }
 
   protected onDispose(): void {
     ReactDom.unmountComponentAtNode(this.domElement);
-  }
-
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {

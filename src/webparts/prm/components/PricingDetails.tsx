ca586a8cba304d4a77fb225cb913605c08000ae3
rezmes <rezmes@gmail.com>
2025-02-15 14:05:09 +0330

    Bilingual

--- src/webparts/prm/components/PricingDetails.tsx
+++ src/webparts/prm/components/PricingDetails.tsx
@@ -8,6 +8,10 @@ import {
 } from "office-ui-fabric-react";
 import GenericDropdown from "./GenericDropdown";
 
+import * as strings from 'MyWebPartStrings';
+
+
+
 interface IPricingDetailsProps {
   label: string;
   field: string;
@@ -32,104 +36,207 @@ interface IPricingDetailsProps {
 }
 
 class PricingDetails extends React.Component<IPricingDetailsProps> {
-  renderTable() {
-    const {
-      field,
-      options,
-      assessment,
-      index,
-      handleDropdownChange,
-      handleInputChange,
-      addRow,
-      removeRow,
-      label,
-    } = this.props;
+//   renderTable() {
+//     const {
+//       field,
+//       options,
+//       assessment,
+//       index,
+//       handleDropdownChange,
+//       handleInputChange,
+//       addRow,
+//       removeRow,
+//       label,
+//     } = this.props;
 
-    return (
-      <table>
-        <tbody>
+//     return (
+//       <table>
+//         <tbody>
+//           <tr>
+//             <th>{label}</th>
+//             <th>Quantity</th>
+//             <th>Price Per Unit</th>
+//             <th>Total Cost</th>
+//           </tr>
+//           {Array.isArray(assessment[field]) && assessment[field].length > 0 ? (
+//             assessment[field].map((item: any, partIndex: number) => {
+//               const totalCost = item.quantity * item.pricePerUnit;
+//               return (
+//                 <tr key={partIndex}>
+//                   <td>
+//                     <GenericDropdown
+//                       label={`${label} ${partIndex + 1}`}
+//                       options={options}
+//                       selectedKey={item.item ? item.item.key : undefined}
+//                       onChanged={(option) =>
+//                         handleDropdownChange(field, option!, index, partIndex)
+//                       }
+//                     />
+//                   </td>
+//                   <td>
+//                     <TextField
+//                       value={item.quantity.toString()}
+//                       onChanged={(newValue: string) =>
+//                         handleInputChange(
+//                           newValue,
+//                           "quantity",
+//                           index,
+//                           partIndex,
+//                           field
+//                         )
+//                       }
+//                       type="number"
+//                     />
+//                   </td>
+//                   <td>
+//                     <TextField
+//                       value={item.pricePerUnit.toString()}
+//                       onChanged={(newValue: string) =>
+//                         handleInputChange(
+//                           newValue,
+//                           "pricePerUnit",
+//                           index,
+//                           partIndex,
+//                           field
+//                         )
+//                       }
+//                       type="number"
+//                     />
+//                   </td>
+//                   <td>{totalCost.toFixed(0)}</td>
+//                   <td>
+//                     <PrimaryButton
+//                       text="Remove"
+//                       onClick={() => removeRow(field, index, partIndex)}
+//                     />
+//                   </td>
+//                 </tr>
+//               );
+//             })
+//           ) : (
+//             <tr>
+//               <td colSpan={5}>No {label.toLowerCase()} added yet.</td>
+//             </tr>
+//           )}
+//         </tbody>
+//       </table>
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.renderTable()}
+//         <PrimaryButton
+//           text={`Add ${this.props.label}`}
+//           onClick={() => this.props.addRow(this.props.field, this.props.index)}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default PricingDetails;
+// PricingDetails.tsx
+renderTable() {
+  const {
+    field,
+    options,
+    assessment,
+    index,
+    handleDropdownChange,
+    handleInputChange,
+    addRow,
+    removeRow,
+    label,
+  } = this.props;
+
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>{label}</th>
+          <th>{strings.Quantity}</th>
+          <th>{strings.PricePerUnit}</th>
+          <th>{strings.TotalCost}</th>
+        </tr>
+        {Array.isArray(assessment[field]) && assessment[field].length > 0 ? (
+          assessment[field].map((item: any, partIndex: number) => {
+            const totalCost = item.quantity * item.pricePerUnit;
+            return (
+              <tr key={partIndex}>
+                <td>
+                  <GenericDropdown
+                    label={`${label} ${partIndex + 1}`}
+                    options={options}
+                    selectedKey={item.item ? item.item.key : undefined}
+                    onChanged={(option) =>
+                      handleDropdownChange(field, option!, index, partIndex)
+                    }
+                  />
+                </td>
+                <td>
+                  <TextField
+                    value={item.quantity.toString()}
+                    onChanged={(newValue: string) =>
+                      handleInputChange(
+                        newValue,
+                        "quantity",
+                        index,
+                        partIndex,
+                        field
+                      )
+                    }
+                    type="number"
+                  />
+                </td>
+                <td>
+                  <TextField
+                    value={item.pricePerUnit.toString()}
+                    onChanged={(newValue: string) =>
+                      handleInputChange(
+                        newValue,
+                        "pricePerUnit",
+                        index,
+                        partIndex,
+                        field
+                      )
+                    }
+                    type="number"
+                  />
+                </td>
+                <td>{totalCost.toFixed(0)}</td>
+                <td>
+                  <PrimaryButton
+                    text={strings.Remove}
+                    onClick={() => removeRow(field, index, partIndex)}
+                  />
+                </td>
+              </tr>
+            );
+          })
+        ) : (
           <tr>
-            <th>{label}</th>
-            <th>Quantity</th>
-            <th>Price Per Unit</th>
-            <th>Total Cost</th>
+            <td colSpan={5}>{`${strings.No} ${label.toLowerCase()} ${strings.AddedYet}`}</td>
           </tr>
-          {Array.isArray(assessment[field]) && assessment[field].length > 0 ? (
-            assessment[field].map((item: any, partIndex: number) => {
-              const totalCost = item.quantity * item.pricePerUnit;
-              return (
-                <tr key={partIndex}>
-                  <td>
-                    <GenericDropdown
-                      label={`${label} ${partIndex + 1}`}
-                      options={options}
-                      selectedKey={item.item ? item.item.key : undefined}
-                      onChanged={(option) =>
-                        handleDropdownChange(field, option!, index, partIndex)
-                      }
-                    />
-                  </td>
-                  <td>
-                    <TextField
-                      value={item.quantity.toString()}
-                      onChanged={(newValue: string) =>
-                        handleInputChange(
-                          newValue,
-                          "quantity",
-                          index,
-                          partIndex,
-                          field
-                        )
-                      }
-                      type="number"
-                    />
-                  </td>
-                  <td>
-                    <TextField
-                      value={item.pricePerUnit.toString()}
-                      onChanged={(newValue: string) =>
-                        handleInputChange(
-                          newValue,
-                          "pricePerUnit",
-                          index,
-                          partIndex,
-                          field
-                        )
-                      }
-                      type="number"
-                    />
-                  </td>
-                  <td>{totalCost.toFixed(0)}</td>
-                  <td>
-                    <PrimaryButton
-                      text="Remove"
-                      onClick={() => removeRow(field, index, partIndex)}
-                    />
-                  </td>
-                </tr>
-              );
-            })
-          ) : (
-            <tr>
-              <td colSpan={5}>No {label.toLowerCase()} added yet.</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
+        )}
+      </tbody>
+    </table>
+  );
+}
 
-  render() {
-    return (
-      <div>
-        {this.renderTable()}
-        <PrimaryButton
-          text={`Add ${this.props.label}`}
-          onClick={() => this.props.addRow(this.props.field, this.props.index)}
-        />
-      </div>
-    );
-  }
+render() {
+  return (
+    <div>
+      {this.renderTable()}
+      <PrimaryButton
+        text={`${strings.Add} ${this.props.label}`}
+        onClick={() => this.props.addRow(this.props.field, this.props.index)}
+      />
+    </div>
+  );
+}
+
 }
 
 export default PricingDetails;
